92b857764b219b7089a517fee3a063db5117307b
davidbabaev <davidbabaev198@gmail.com>
2026-04-24 14:31:21 +0300

    start fixing the socketService file to work dynamically with auth token, and not froze token on login/ regiter, it cause bug in the chat that the user can't chatting because the socket have stale token, untill the refresh and get the current user token the chat will not work, i just started workign on the code to solve this, not finished yet

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,14 +1,31 @@
 import { io } from "socket.io-client";
 const SERVER_URL = 'http://localhost:8181';
-const socket = io(SERVER_URL, {
+/* const socket = io(SERVER_URL, {
     auth: (cb) => {
         cb({token: localStorage.getItem('auth-token')})
     }
-});
+}); */
 
-socket.on('connect', () => {
-    console.log('Connected to server:', socket.id);
-})
+let socket = null;
+
+export function connectSocket(){
+    if(socket?.connected) return socket;
+
+    socket = io(SERVER_URL, {
+        auth: (cb) => {
+            cb({token: localStorage.getItem('auth-token')})
+        }
+    })
+
+    socket.on('connect', () => {
+        console.log('Connected to server:', socket.id);
+    })
+
+
+    return socket
+}
+
+
 
 socket.on('connect_error', (error) => {
     console.log('Connection error:', error.message);
